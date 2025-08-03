e16b07af32da634667b1f4c8f05e9feee514110d
ray0189 <65903268+ray0189@users.noreply.github.com>
2025-08-03 19:15:46 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 require('dotenv').config();
-const express = require("express");
+const express = require('express');
 const app = express();
-const addonInterface = require("./addon");
+const addonInterface = require('./addon');
 
-app.get("/manifest.json", (req, res) => {
-    res.send(addonInterface.manifest);
+app.get('/manifest.json', (req, res) => {
+  res.send(addonInterface.manifest);
 });
 
-app.get("/:resource/:type/:id", (req, res) => {
-    addonInterface.get(req.params.resource, req.params.type, req.params.id)
-        .then(resp => res.send(resp))
-        .catch(err => res.status(500).send({ err: err.message }));
+app.get('/:resource/:type/:id', (req, res) => {
+  addonInterface.get(req.params.resource, req.params.type, req.params.id)
+    .then(resp => res.send(resp))
+    .catch(err => res.status(500).send({ err: err.message }));
 });
 
-app.listen(7000, () => console.log("Addon running on :7000"));
+module.exports = app;
